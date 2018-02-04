2a344a54ee8d8217adac010c3b7a5cabfbaea2b5
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-02-04 23:00:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,7 @@ function destructivelyAddElementToEndOfArray(array, element){
     }
     
     function removeElementFromEndOfArray(array, element){
-      return array.slice(-1);
+      return array.slice(array.length[-1]);
     }
       
       
