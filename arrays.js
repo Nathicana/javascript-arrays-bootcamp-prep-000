95bc3d68e8662d747404cbf63f265b7b2a144101
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-02-04 22:16:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -48,7 +48,7 @@ function destructivelyAddElementToEndOfArray(array, element){
       
       
       function accessElementInArray(array, index){
-        console.log(array[3]);
+       return(array[index]);
 
       }
       
